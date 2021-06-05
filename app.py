8b425c19acb9f77f8fbd6c4d3ba3262cf5946d2b
smcgdub <stephen_xyz1@hotmail.com>
2021-06-05 19:12:41 +0000

    Have created the profile page

--- app.py
+++ app.py
@@ -56,6 +56,8 @@ def register():
         # START USER IN A SESSION
         session["user"] = request.form.get("username").lower()
         flash("You have been succesfully registered")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -72,6 +74,7 @@ def login():
                     existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome back {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
             else:
                 # INCORRECT PASSWORD
                 flash("Username and/or password is incorrect")
@@ -83,6 +86,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
