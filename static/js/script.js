b6d6d3fc004eb2ce9ed4979d35ce49d7d46dbf1c
smcgdub <stephen_xyz1@hotmail.com>
2021-06-06 18:41:49 +0000

    Created drinks cards and synced up drink dats from mongo so it is displaying in each card

--- static/js/script.js
+++ static/js/script.js
@@ -12,6 +12,17 @@ $(document).ready(function () {
   $('.datepicker').datepicker({
     format: 'dd mmm yyyy',
     yearRange: [1900, currYear - 0],
-    setDefaultDate: new Date(01,01,2021)
+    setDefaultDate: new Date(01, 01, 2021)
   });
 });
+
+// DRINKS CARD
+$(document).ready(function () {
+  $('.collapsible').collapsible();
+});
+
+
+// AUTOMATICALLY INIT ALL JS WITH FUNCTION BELOW
+// document.addEventListener('DOMContentLoaded', function () {
+//   M.AutoInit();
+// });
